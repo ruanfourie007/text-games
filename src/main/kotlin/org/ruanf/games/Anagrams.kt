1afc776f8571f8ca4a2bd4a0c714236f37dfebc2
ruan-fourie-gcx <ruan.fourie@grandcentrix.net>
2024-12-10 09:13:16 +0100

    Re-ordered components of Anagram for readability.

--- src/main/kotlin/org/ruanf/games/Anagrams.kt
+++ src/main/kotlin/org/ruanf/games/Anagrams.kt
@@ -3,54 +3,6 @@ package org.ruanf.games
 import org.ruanf.Game
 import kotlin.system.exitProcess
 
-class Subject(private val phrase: String) {
-    private val signature = AnagramService.createSignature(phrase)
-    operator fun component1(): String {
-        return phrase
-    }
-    operator fun component2(): String {
-        return signature
-    }
-}
-
-object AnagramService {
-    private const val MAX_PHRASE_LENGTH = 100
-    /* We are dropping punctuation and symbol characters instead of the usual ^a-z matching,
-    this is intended to better support multiple languages. */
-    private val PHRASE_SANITIZE_REGEX = "[\\p{P}\\p{S}]".toRegex()
-
-    private val history: HashMap<String, MutableSet<String>> = HashMap()
-
-    fun sanitizePhrase(phrase: String): String {
-        return phrase.replace(PHRASE_SANITIZE_REGEX, "")
-    }
-
-    fun validatePhrase(phrase: String) {
-        require(phrase.length <= MAX_PHRASE_LENGTH) {
-            "A phrase cannot exceed $MAX_PHRASE_LENGTH characters."
-        }
-    }
-
-    fun createSignature(phrase: String): String {
-        return phrase.replace(" ", "").lowercase().toCharArray().sortedArray().joinToString(separator="")
-    }
-
-    fun getAnagramsOfSubject(subject: Subject): List<String> {
-        val (phrase, signature) = subject
-        return history[signature]?.filter { it.lowercase() != phrase.lowercase() }?.distinctBy { it.lowercase() } ?: emptyList()
-    }
-
-    fun addToHistory(subject: Subject) {
-        val (phrase, signature) = subject
-
-        if (history.containsKey(signature)) {
-            history[signature]?.add(phrase)
-        } else {
-            history[signature] = mutableSetOf(phrase)
-        }
-    }
-}
-
 class AnagramGame: Game {
 
     override fun title(): String {
@@ -112,6 +64,54 @@ class AnagramGame: Game {
                 previousPhrase = phrase
             }
         }
+    }
+}
+
+object AnagramService {
+    private const val MAX_PHRASE_LENGTH = 100
+    /* We are dropping punctuation and symbol characters instead of the usual ^a-z matching,
+    this is intended to better support multiple languages. */
+    private val PHRASE_SANITIZE_REGEX = "[\\p{P}\\p{S}]".toRegex()
+
+    private val history: HashMap<String, MutableSet<String>> = HashMap()
+
+    fun sanitizePhrase(phrase: String): String {
+        return phrase.replace(PHRASE_SANITIZE_REGEX, "")
+    }
+
+    fun validatePhrase(phrase: String) {
+        require(phrase.length <= MAX_PHRASE_LENGTH) {
+            "A phrase cannot exceed $MAX_PHRASE_LENGTH characters."
+        }
+    }
+
+    fun createSignature(phrase: String): String {
+        return phrase.replace(" ", "").lowercase().toCharArray().sortedArray().joinToString(separator="")
+    }
+
+    fun getAnagramsOfSubject(subject: Subject): List<String> {
+        val (phrase, signature) = subject
+        return history[signature]?.filter { it.lowercase() != phrase.lowercase() }?.distinctBy { it.lowercase() } ?: emptyList()
+    }
+
+    fun addToHistory(subject: Subject) {
+        val (phrase, signature) = subject
+
+        if (history.containsKey(signature)) {
+            history[signature]?.add(phrase)
+        } else {
+            history[signature] = mutableSetOf(phrase)
+        }
+    }
+}
+
+class Subject(private val phrase: String) {
+    private val signature = AnagramService.createSignature(phrase)
+    operator fun component1(): String {
+        return phrase
+    }
+    operator fun component2(): String {
+        return signature
     }
 }
 
